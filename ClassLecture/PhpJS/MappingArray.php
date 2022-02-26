7a96abfd8892699de54840ecfd951ce19b1c2956
rakib3004 <bsse1129@iit.du.ac.bd>
2022-02-26 19:20:39 +0600

    add mapping array

---
+++ ClassLecture/PhpJS/MappingArray.php
@@ -0,0 +1,12 @@
+<?php
+
+
+$foodItemPrice= array('Fride Rice'=>70,'Chicken'=>140,
+'Mutton'=>240,'Carry'=>40,'Nodols'=>120,'Nan'=>10,'Mashroom'=>150);
+
+
+echo $foodItemPrice['Nodols'].'<br>';
+
+echo $foodItemPrice['Chicken'].'<br>';
+
+?>
