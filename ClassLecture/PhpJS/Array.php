b0f32521fe89baff3f58f822b5faf85d30db3cf9
rakib3004 <bsse1129@iit.du.ac.bd>
2022-02-26 18:01:36 +0600

    create array and multidimensional array and print them

---
+++ ClassLecture/PhpJS/Array.php
@@ -0,0 +1,14 @@
+<?php
+
+$numberOfCommit=array(14,65,14,32,24,13,6,2,48,6,1,3,4,64,8);
+
+echo($numberOfCommit[6]."</br>");
+
+echo($numberOfCommit[4]."</br>");
+
+echo($numberOfCommit[8]."</br>");
+
+echo($numberOfCommit[12]."</br>");
+
+
+?>
---
+++ ClassLecture/PhpJS/MultiDimensionalArray.php
@@ -0,0 +1,19 @@
+<?php
+
+
+$collectPoint= array(
+    array(250,350,600),
+    array(650,500,750),
+    array(1000,900,1000)
+);
+
+echo($collectPoint[0][2]."</br>");
+
+echo($collectPoint[1][2]."</br>");
+
+echo($collectPoint[1][1]."</br>");
+
+
+
+
+?>
